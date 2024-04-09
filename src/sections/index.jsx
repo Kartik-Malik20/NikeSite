dec84cf485466e3f1e1257204c1678634c0c878f
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-09 11:53:50 +0530

    first commit

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Customerreviews from "./Customerreviews";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+
+export{
+    Hero,
+    Customerreviews,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+    Footer,
+}
